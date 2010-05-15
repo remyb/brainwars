4ab5a6100a790e3c03cad0063db51cabf0a70bb8
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-14 20:28:09 -1000

    refactored title using helpers and added style sheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
