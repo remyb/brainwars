f28a5448fdf56824e33ae88dcee54f9fd631b83e
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-14 23:37:34 -1000

    annotated models plugin addition

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+# Schema version: 20100515091712
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'spec_helper'
+
+describe User do
+  before(:each) do
+    @valid_attributes = {
+      :name => "value for name",
+      :email => "value for email"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@valid_attributes)
+  end
+end
