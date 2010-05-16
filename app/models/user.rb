838075a119291c4affd1fb56157b0fb185da12f1
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-15 23:32:17 -1000

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 # == Schema Information
-# Schema version: 20100515212626
+# Schema version: 20100516072357
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
