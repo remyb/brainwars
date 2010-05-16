c186bed7844ccf5d140ce1cb0c2c400033ad91ee
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-15 19:50:50 -1000

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.help '/help', :controller => 'pages', :action => 'help'
   map.root :controller => 'pages', :action => 'home'
   map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100515091712) do
+ActiveRecord::Schema.define(:version => 20100515212626) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -17,5 +17,7 @@ ActiveRecord::Schema.define(:version => 20100515091712) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
