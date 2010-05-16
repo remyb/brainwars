5adafde94c4ce349bd7125954749fe189fe5d952
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-16 03:19:57 -1000

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -43,4 +43,49 @@ describe UsersController do
       response.should have_tag("title", /Sign up/)
     end
   end
+  
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+          :password_confirmation => "" }
+        @user = Factory.build(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(false)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_tag("title", /sign up/i)
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com", 
+          :password => "foobar",
+          :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
+  end
 end
