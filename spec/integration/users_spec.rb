1c16b77662268d24c366865d0804884d7f712c0f
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-16 18:45:06 -1000

    added users_spec

---
+++ spec/integration/users_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Users" do
+  
+  describe "signup" do
+    
+    describe "failure" do
+      
+      it "should not make a new user" do
+        lambda do
+          visit signup_url
+          click_button
+          response.should render_template('users/new')
+          response.should have_tag("div#errorExplanation")
+        end.should_not change(User, :count)
+      end
+    end
+    
+    describe "success" do
+      
+      it "should make a new user" do
+        lambda do
+          visit signup_url
+          fill_in "Name", :with => "Example User"
+          fill_in "Email", :with => "user@example.com"
+          fill_in "Password", :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
+          click_button
+          response.should render_template('users/show')
+          response.should have_tag("div.flash.success")
+        end.should change(User, :count).by(1)
+      end
+    end
+  end
+end
