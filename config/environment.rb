26a24d1d8a982c3ff066a77e8de84fe8c69cef2d
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-20 00:23:34 -1000

    added hoptoad notifier for exceptions

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,7 @@ Rails::Initializer.run do |config|
   
   config.gem 'will_paginate'
   config.gem 'searchlogic'
+  config.gem 'hoptoad_notifier'
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '59c99a8a891967aa3c847ba0a1d25e84'
+end
