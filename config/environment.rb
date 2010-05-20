b956de9ecd37cda736fb3e46ef4c6e270025dec9
Remy Baumgarten <remy.baumgarten@gmail.com>
2010-05-19 23:16:17 -1000

    added searchlogic

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   
   config.gem 'will_paginate'
+  config.gem 'searchlogic'
   
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
